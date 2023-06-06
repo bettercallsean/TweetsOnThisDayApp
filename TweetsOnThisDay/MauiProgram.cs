91178134e621c70aecc4913d167896afe6cf2a80
Sean Edwards <hello@seanedwards.dev>
2023-06-06 21:01:43 +0100

    Add settings menu

--- TweetsOnThisDay/MauiProgram.cs
+++ TweetsOnThisDay/MauiProgram.cs
@@ -1,41 +1,38 @@
 ﻿using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using TweetsOnThisDay.Data;
 
 namespace TweetsOnThisDay;
 
 public static class MauiProgram
 {
-    public static MauiApp CreateMauiApp()
-    {
-        var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            });
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
 
-        builder.Services.AddMauiBlazorWebView();
+		builder.Services.AddMauiBlazorWebView();
 
-        var a = Assembly.GetExecutingAssembly();
-        using var stream = a.GetManifestResourceStream("TweetsOnThisDay.appsettings.json");
+		var a = Assembly.GetExecutingAssembly();
+		using var stream = a.GetManifestResourceStream("TweetsOnThisDay.appsettings.json");
 
-        var config = new ConfigurationBuilder()
-                    .AddJsonStream(stream)
-                    .Build();
+		var config = new ConfigurationBuilder()
+					.AddJsonStream(stream)
+					.Build();
 
 
-        builder.Configuration.AddConfiguration(config);
+		builder.Configuration.AddConfiguration(config);
 
 #if DEBUG
-        builder.Services.AddBlazorWebViewDeveloperTools();
-        builder.Logging.AddDebug();
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
 #endif
 
-        builder.Services.AddSingleton<WeatherForecastService>();
-
-        return builder.Build();
-    }
+		return builder.Build();
+	}
 }
