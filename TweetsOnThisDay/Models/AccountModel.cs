8eea7bfea4c22ee83e4a8e527b7e6900bb5f8b08
Sean Edwards <hello@seanedwards.dev>
2023-06-10 15:59:29 +0100

    Create AccountModel

---
+++ TweetsOnThisDay/Models/AccountModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TweetsOnThisDay.Models;
+public record Account
+{
+	[JsonPropertyName("email")]
+	public string Email { get; set; }
+
+	[JsonPropertyName("createdVia")]
+	public string CreatedVia { get; set; }
+
+	[JsonPropertyName("username")]
+	public string Username { get; set; }
+
+	[JsonPropertyName("accountId")]
+	public string AccountId { get; set; }
+
+	[JsonPropertyName("createdAt")]
+	public string CreatedAt { get; set; }
+
+	[JsonPropertyName("accountDisplayName")]
+	public string AccountDisplayName { get; set; }
+}
+
+public record AccountRoot
+{
+	[JsonPropertyName("account")]
+	public Account Account { get; set; }
+}
