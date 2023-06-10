429bd7cd5631af6baf4a9f9c235a45095c08d2e6
Sean Edwards <hello@seanedwards.dev>
2023-06-10 12:54:42 +0100

    Create helper class for background colours

---
+++ TweetsOnThisDay/Helpers/BackgroundColourHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace TweetsOnThisDay.Helpers;
+internal static class BackgroundColourHelper
+{
+    public static List<string> GetBackgroundColoursList() => new()
+    {
+                "skyblue",
+                "lightcoral",
+                "mediumspringgreen",
+                "lightsalmon",
+                "palegreen",
+                "thistle"
+            };
+}
