26cb22d0b68e5a5aea2906c8b09f7fe7d85f5741
Sean Edwards <hello@seanedwards.dev>
2023-06-12 18:57:24 +0100

    Add using statement

--- TweetsOnThisDay/Services/ZipService.cs
+++ TweetsOnThisDay/Services/ZipService.cs
@@ -1,25 +1,27 @@
-﻿namespace TweetsOnThisDay.Services;
+﻿using TweetsOnThisDay.Models;
+
+namespace TweetsOnThisDay.Services;
 internal class ZipService
 {
 
-	public static async Task<List<Models.ZipEntry>> ExtractFiles(string zipFilePath)
-	{
-		var entries = new List<Models.ZipEntry>();
+    public static async Task<List<ZipEntry>> ExtractFiles(string zipFilePath)
+    {
+        var entries = new List<ZipEntry>();
 
-		using var zip = Ionic.Zip.ZipFile.Read(zipFilePath);
+        using var zip = Ionic.Zip.ZipFile.Read(zipFilePath);
 
-		var foo = zip.Where(x => x.FileName is "data/tweets.js" or "data/account.js" || (x.FileName.Contains("data/profile_media") && x.FileName.EndsWith(".jpg")));
+        var files = zip.Where(x => x.FileName is "data/tweets.js" or "data/account.js" || (x.FileName.Contains("data/profile_media") && x.FileName.EndsWith(".jpg")));
 
-		foreach (var file in foo)
-		{
-			using var ms = new MemoryStream();
+        foreach (var file in files)
+        {
+            using var ms = new MemoryStream();
 
-			await using var fileStream = file.OpenReader();
-			await fileStream.CopyToAsync(ms);
+            await using var fileStream = file.OpenReader();
+            await fileStream.CopyToAsync(ms);
 
-			entries.Add(new Models.ZipEntry { Name = file.FileName, IsImage = file.FileName.EndsWith(".jpg"), FileBytes = ms.ToArray() });
-		}
+            entries.Add(new ZipEntry { Name = file.FileName, IsImage = file.FileName.EndsWith(".jpg"), FileBytes = ms.ToArray() });
+        }
 
-		return entries;
-	}
+        return entries;
+    }
 }
